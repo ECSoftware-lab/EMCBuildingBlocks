f35c91e3aaeacff1d4431f30a7309adf05e2c342
Esteban <cuezzo.esteban@gmail.com>
2025-08-12 12:31:37 -0300

    vamos bien

--- EMC.BuildingBlocks/Repository/IAsyncRepository.cs
+++ EMC.BuildingBlocks/Repository/IAsyncRepository.cs
@@ -5,13 +5,13 @@ namespace EMC.BuildingBlocks.Repository
 {
     public interface IAsyncRepository<T,TId> where T : class, IEntityWithId<TId>
     {
+        Task<T> GetByIdAsync(TId id, bool disableTracking = true);
         Task<IReadOnlyList<T>> GetAllAsync(bool disableTracking = true);
         Task<PaginatedResult<T>> ToPaginatedResultAsync(IQueryable<T> query, BasePaginationRequest request, string observation = null, CancellationToken cancellationToken = default);
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         List<Expression<Func<T, object>>> includes = null,
-                                        bool disableTracking = true);
-        Task<T> GetByIdAsync(TId id, bool disableTracking = true);
+                                        bool disableTracking = true);     
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task DeleteAsync(T entity);
@@ -23,21 +23,6 @@ namespace EMC.BuildingBlocks.Repository
         List<Expression<Func<T, object>>> includes = null,
         bool disableTracking = true);
 
-        /*  
-Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
-
-Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
-Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-string includeString = null,
-bool disableTracking = true);
-
-
-
-Task<T> GetByIdAsync(int id, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true);
-
-
-void DetachEntityIfTracked<TEntity>(TEntity entity) where TEntity : class;
-
-*/
+      
     }
 }
