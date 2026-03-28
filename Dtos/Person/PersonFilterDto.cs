8a55f4688bffbb169df83df2c2c62b5c5140e994
EMC\Esteban <emcuezzo>
2026-03-28 15:32:01 -0300

    antes de migrar a net 10

---
+++ Dtos/Person/PersonFilterDto.cs
@@ -0,0 +1,17 @@
+﻿using EMC.BuildingBlocks.Application.Pagination;
+
+namespace EMC.BuildingBlocks.Dtos.Person
+{
+    public record PersonFilterDto : BasePaginationRequest
+    {
+        public string CompanyId { get; set; }
+        public Dictionary<string, string> AttributeFilters { get; set; } = new();
+     
+        public bool? IsUnique { get; set; }
+
+        public List<string>? PersonType { get; set; }
+        public int PersonTypeEfect { get; set; }
+        public DateTime? CreateIn { get; set; }
+        public DateTime? CreateEnd { get; set; }
+    }
+}
