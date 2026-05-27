7db9400d06e03893b1eaf2c3c5a83764906a95cd
EMC\Esteban <emcuezzo>
2026-05-27 13:08:19 -0300

    Fix Building

---
+++ EventBus/IntegrationEvents/UserEvents/OneEmailSendCodeEvent.cs
@@ -0,0 +1,14 @@
+﻿using EMC.BuildingBlocks.EventBus;
+
+namespace EMC.BuildingBlocks.EventBus.IntegrationEvents.UserEvents
+{
+    public class OneEmailSendCodeEvent : IntegrationEvent
+    {
+        public string Email { get; set; }
+        public string DisplayName { get; set; } // opcional, si lo tenés
+        public string ConfirmationLink { get; set; }
+        public string Template { get; set; } // puede ser el HTML o el nombre de plantilla
+        public int ExpiresIn { get; set; }
+    }
+
+}
