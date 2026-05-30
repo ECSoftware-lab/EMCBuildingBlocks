5b0a0d6eb500941de2eda316bde1295a5e53f873
EMC\Esteban <emcuezzo>
2026-05-29 21:31:44 -0300

    fix

---
+++ Settings/MetaSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace EMC.BuildingBlocks.Settings
+{
+    public class MetaSettings
+    {
+        public string AppId { get; set; } = "";
+        public string AppSecret { get; set; } = "";
+        public string RedirectUri { get; set; } = "";
+        public string ApiVersion { get; set; } = "v25.0";
+        public string Ambiente { get; set; } = "dev";  // viene de appsettings
+
+        public string AuthBaseUrl =>
+            $"https://www.facebook.com/{ApiVersion}/dialog/oauth";
+        public string TokenUrl =>
+            $"https://graph.facebook.com/{ApiVersion}/oauth/access_token";
+    }
+}
